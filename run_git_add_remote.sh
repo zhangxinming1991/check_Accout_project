f1593a8cbd83d606766a7ee1ae2a7b930a69a355
simon <13755074942@163.com>
2016-11-24 15:43:20 +0800

    规整版本3.6

--- run_git_add_remote.sh
+++ run_git_add_remote.sh
@@ -4,6 +4,6 @@
 #	exit -1
 #fi
 git remote rm origin 
-git remote add origin git@119.29.235.201:/home/git/check_Accout_Project.git
-git remote set-url origin ssh://git@119.29.235.201:20029/home/git/check_Accout_Project.git
+git remote add origin git@119.29.235.201:/home/git/check_Accout_project.git
+git remote set-url origin ssh://git@119.29.235.201:20029/home/git/check_Accout_project.git
 git remote show origin
